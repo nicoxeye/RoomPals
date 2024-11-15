33bada97287ba2855876d414664a58bec01ee36e
Yola <jolamajcher12@gmail.com>
2024-11-15 20:02:34 +0100

    Added Location, Room class + Updated Student class

--- RoomPals/Classes/Student.cs
+++ RoomPals/Classes/Student.cs
@@ -1,19 +1,24 @@
-﻿using System;
+﻿using RoomPals.Classes;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace RoomPals.Classes
 {
-    public class Student
+    public class Student:Location
     {
         public string Name { get; set; }
         public string Surname { get; set; }
         public int Age { get; set; }
+
+        public DateTime BirthDate { get; private set; }
         public string Major { get; set; }
         public string Sex { get; set; }
-        public string City { get; set; }
         public string NightOrDay { get; set; }
         public string DogOrCat { get; set; }
         public string PartyOrBook { get; set; }
@@ -23,19 +28,23 @@ namespace RoomPals.Classes
 
         // New properties for login
         public string Username { get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
 
+        // Regex patterns
+        string pattern_email = "@^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+        string pattern_password = "@^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d).{8,16}$";  //requirements: password must contain: min 1 uppercase letter, min 1 lowercase letter, min 1 digit 
+
         // Constructor to initialize the student object
-        public Student(string name, string surname, int age, string major, string sex, string city,
+        public Student(string name, string surname, int age, DateTime birthDate, string major, string sex,
                       string nightOrDay, string dogOrCat, string partyOrBook, string activeOrPassive,
-                      string mainLanguage, string secondLanguage, string username, string password)
+                      string mainLanguage, string secondLanguage, string username, string email, string password, string Country, string City):base(Country, City)
         {
             Name = name;
             Surname = surname;
             Age = age;
             Major = major;
             Sex = sex;
-            City = city;
             NightOrDay = nightOrDay;
             DogOrCat = dogOrCat;
             PartyOrBook = partyOrBook;
@@ -43,6 +52,15 @@ namespace RoomPals.Classes
             MainLanguage = mainLanguage;
             SecondLanguage = secondLanguage;
             Username = username;
+            if (!Regex.IsMatch(email, pattern_email))
+            {
+                throw new ArgumentException("Incorrect email format");
+            }
+            Email = email;
+            if (!Regex.IsMatch(password, pattern_password))
+            {
+                throw new Exception("Incorrect password format");
+            }
             Password = password;
         }
 
@@ -54,7 +72,10 @@ namespace RoomPals.Classes
 
         public override string ToString()
         {
-            return $"{Name} {Surname}, Age: {Age}, Major: {Major}, Sex: {Sex}, City: {City}, Preferences: {NightOrDay}, {DogOrCat}, {PartyOrBook}, Activity: {ActiveOrPassive}, Languages: {MainLanguage}, {SecondLanguage}";
+            return $"{Name} {Surname}, Age: {Age}, Major: {Major}, Sex: {Sex}, City: {city}, Preferences: {NightOrDay}, {DogOrCat}, {PartyOrBook}, Activity: {ActiveOrPassive}, Languages: {MainLanguage}, {SecondLanguage}";
         }
     }
 }
+
+
+
