3de5dfff83d860d3e655f173a6d6dda3f00fd298
Yola <jolamajcher12@gmail.com>
2024-11-16 22:31:08 +0100

    Added new Styles, Updated SignInWindow (not finished yet, workin on it :))

--- RoomPals/SignInWindow.xaml.cs
+++ RoomPals/SignInWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace RoomPals
 {
@@ -22,6 +11,80 @@ namespace RoomPals
         public SignInWindow()
         {
             InitializeComponent();
+        }
+
+        private void Email_Click(object sender, RoutedEventArgs e)
+        {
+            if (EmailTextBox.Visibility == Visibility.Collapsed)
+            {
+                EmailTextBox.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                EmailTextBox.Visibility = Visibility.Collapsed;
+            }
+            ////sprawdzenie:
+            //if (EmailTextBox.Visibility == Visibility.Collapsed)
+            //{
+            //    EmailTextBox.Visibility = Visibility.Visible;
+            //    MessageBox.Show("TextBox is now visible."); // Debug: sprawdzenie
+            //}
+            //else
+            //{
+            //    EmailTextBox.Visibility = Visibility.Collapsed;
+            //    MessageBox.Show("TextBox is now collapsed."); // Debug: sprawdzenie
+            //}
+
+        }
+        private void EmailTextBoxKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                string enteredEmail = EmailTextBox.Text;
+                MessageBox.Show(enteredEmail);
+                // EmailTextBox.Visibility = Visibility.Collapsed;
+
+                this.Focus();
+                
+            }
+
+        }
+        private void Username_Click(object sender, RoutedEventArgs e)
+        {
+            if (UsernameTextBox.Visibility == Visibility.Collapsed)
+            {
+                UsernameTextBox.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                UsernameTextBox.Visibility = Visibility.Collapsed;
+            }
+        }
+        private void Password_Click(object sender, RoutedEventArgs e)
+        {
+            if (PasswordBox.Visibility == Visibility.Collapsed)
+            {
+                PasswordBox.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                PasswordBox.Visibility = Visibility.Collapsed;
+            }
+        }
+        private void RepeatPassword_Click(object sender, RoutedEventArgs e)
+        {
+            if (RepeatPasswordBox.Visibility == Visibility.Collapsed)
+            {
+                RepeatPasswordBox.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                RepeatPasswordBox.Visibility= Visibility.Collapsed;
+            }
+        }
+        private void CreateAccount_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
