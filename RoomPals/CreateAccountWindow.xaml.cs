1ab4766e253735f0c62671caea3acc3b8bd25634
wera <ciastkouni@gmail.com>
2024-11-22 19:58:44 +0100

    made CreateAccountWindow
    added background, basic TextBoxes for user input and a Button that leads to ChooseTownWindow (for now its blank)

---
+++ RoomPals/CreateAccountWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RoomPals
+{
+    public partial class CreateAccountWindow : Window
+    {
+        public CreateAccountWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NameTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseTownWindow chooseTownWindow = new ChooseTownWindow();
+            chooseTownWindow.Show();
+            this.Hide();
+        }
+    }
+}
