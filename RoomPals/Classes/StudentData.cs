135550c015a774bc8c26f0db4a8e2f956617e227
nicoxeye <nicoxeye@op.pl>
2024-11-30 13:22:51 +0100

    create StudentData.cs
    made a short list consisting of 5 students and their info, remember the usernames and the password to test log ins!!!! <3

---
+++ RoomPals/Classes/StudentData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomPals.Classes
+{
+    public static class StudentData
+    {
+        public static List<Student> Students = new List<Student>
+        {
+            new Student("Nikola", "Jamrozy", 19, "IT", "night", "dog", "book", "active", "pl", "esp", "nicoxeye", "nicoxeye@op.pl", "Randompassword1", "myslowice"),
+            new Student("Jola", "Majcher", 19, "IT", "night", "dog", "party", "active", "pl", "eng", "yola", "yola@op.pl", "Randompassword1", "chorzow"),
+            new Student("Weronika", "Miozga", 20, "IT", "night", "dog", "party", "active", "pl", "eng", "jellix", "jellix@op.pl", "Randompassword1", "katowice"),
+            new Student("Julia", "Pawlik", 20, "IT", "night", "cat", "party", "active", "pl", "eng", "judzia", "judzia@op.pl", "Randompassword1", "katowice"),
+            new Student("Jakub", "Jamrozy", 25, "Logistics", "night", "cat", "party", "active", "pl", "none", "jamrox", "jamrox@op.pl","Randompassword1", "myslowice")
+        };
+    }
+}
