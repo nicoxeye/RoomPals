f71fdca1fd9986b976cc0f7cb5915caf7627b06c
wera <ciastkouni@gmail.com>
2024-12-08 00:14:51 +0100

    added functionality behind ChooseSecondLanguage

--- RoomPals/ChooseSecondLanguage.xaml.cs
+++ RoomPals/ChooseSecondLanguage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RoomPals.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,20 +15,47 @@ using System.Windows.Shapes;
 
 namespace RoomPals
 {
-    /// <summary>
-    /// Logika interakcji dla klasy ChooseSecondLanguage.xaml
-    /// </summary>
     public partial class ChooseSecondLanguage : Window
     {
-        public ChooseSecondLanguage()
+        private Student _currentStudent;
+        public ChooseSecondLanguage(Student student)
         {
+            _currentStudent = student;
             InitializeComponent();
-            UserNameTextBlock.Text = "Example:)!";
+            UserNameTextBlock.Text = $"{_currentStudent.Name}";
+            English.Content = "English";
+            Polish.Content = "Polish";
+            German.Content = "German";
+            Spanish.Content = "Spanish";
+            Ukrainian.Content = "Ukrainian";
+            French.Content = "French";
+        }
+
+
+        private void Language_Click(object sender, RoutedEventArgs e)
+        {
+            RadioButton clickedRadioButton = sender as RadioButton;
+            if (clickedRadioButton != null)
+            {
+                string selectedLan2 = clickedRadioButton.Content.ToString();
+                _currentStudent.SecondLanguage = selectedLan2;
+
+            }
         }
 
         private void ConfirmYourChoiceButton_Click(object sender, RoutedEventArgs e)
         {
             // this has to show if in account creation otherwise the user has to exit through the go back click
+            if (string.IsNullOrEmpty(_currentStudent.SecondLanguage))
+            {
+                MessageBox.Show("Please select a language before confirming.");
+                return;
+            }
+
+            StudentData.UpdateStudent(_currentStudent);
+            // last one wooooo 
+            MessageBox.Show($"You have confirmed your choice: {_currentStudent.SecondLanguage}");
+
             QuizWindow quizWindow = new QuizWindow();
             quizWindow.Show();
             this.Close();
@@ -35,6 +63,15 @@ namespace RoomPals
 
         private void go_back_Click(object sender, RoutedEventArgs e)
         {
+            if (string.IsNullOrEmpty(_currentStudent.SecondLanguage))
+            {
+                MessageBox.Show("Please select a language before confirming.");
+                return;
+            }
+
+            StudentData.UpdateStudent(_currentStudent);
+            MessageBox.Show($"You have confirmed your choice: {_currentStudent.SecondLanguage}");
+
             StartWindow startWindow = new StartWindow();
             startWindow.Show();
             this.Close();
